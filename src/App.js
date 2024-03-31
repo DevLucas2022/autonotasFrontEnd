7c0dba51832673db8e6fed1ada775fc87c6d524c
Luis Felipe <luis.lima42@fatec.sp.gov.br>
2024-03-31 19:33:48 -0300

    :tada: initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <Box>
+      <Typography color='black'>Auto Notas</Typography>
+    </Box>
+  );
+}
+
+export default App;
