d77c7944c846775dff265e5fb2fd35805d360ab8
Lucas Santos <lucas.santos517@fatec.sp.gov.br>
2024-04-15 23:06:45 -0300

    :sparkles: create login and signUp aluno and professor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import { Box, Typography } from '@mui/material';
 import './App.css';
+import './components/aluno/loginAluno/SignIn'
+import SignInSideAluno from './components/aluno/loginAluno/SignIn';
+import SignUpAluno from './components/aluno/cadastroAluno/SignUp';
+import SignInSideProfessor from './components/professor/loginProfessor/SignIn';
+import SignUpProfessor from './components/professor/cadastroProfessor/SignUp';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+/* COLOCAR ESSAS ROTAS NO SWITCH <Route exact path="/" component={Home} />
+<Route component={NotFound} /> */
 function App() {
   return (
-    <Box>
-      <Typography color='black'>Auto Notas</Typography>
-    </Box>
+    <SignInSideAluno/>
+  );
+  return (
+    <SignUpAluno/>
+  );
+  return (
+    <SignInSideProfessor/>
+  );
+  return (
+    <SignUpProfessor/>
+  );
+  return (
+    <Router>
+      <div>
+        <Switch>
+ 
+          <Route path="/professorSignIn" component={SignInSideProfessor} />
+          <Route path="/professorSignUp" component={SignUpProfessor} />
+          <Route path="/alunoSignIn" component={SignInSideAluno} />
+          <Route path="/alunoSignUp" component={SignUpAluno} />
+         
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
