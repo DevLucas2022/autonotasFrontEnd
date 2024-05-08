637c06ed11a20c4a1654cdfc04febd5954a9517e
Luis <luis.lima42@fatec.sp.gov.br>
2024-05-08 09:55:59 -0300

    removendo alterações feitas

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import './App.css';
 import React from 'react';
 import RoutesApp from './routes';
-import AppAppBar from './components/home/components/AppAppBar';
-import { Grid } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import getLPTheme from './components/home/getLPTheme';
-
-
 
 
 function App() {
-  const [mode, setMode] = React.useState('light');
-  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
-  const LPtheme = createTheme(getLPTheme(mode));
-  const defaultTheme = createTheme({ palette: { mode } });
-
-  const toggleCustomTheme = () => {
-    setShowCustomTheme((prev) => !prev);
-  };
-
-  const toggleColorMode = () => {
-    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
-  };
   return (
     <>
-      <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
-          </Grid>
-          <Grid item xs={12}>
-            <RoutesApp />
-          </Grid>
-        </Grid>
-      </ThemeProvider>
+      <RoutesApp />
     </>
   );
 }
--- src/components/home/LandingPage.js
+++ src/components/home/LandingPage.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
+import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
+import AppAppBar from './components/AppAppBar';
+import Hero from './components/Hero';
 import LogoCollection from './components/LogoCollection';
 import Highlights from './components/Highlights';
 import Pricing from './components/Pricing';
@@ -13,7 +17,7 @@ import Features from './components/Features';
 import Testimonials from './components/Testimonials';
 import FAQ from './components/FAQ';
 import Footer from './components/Footer';
-import Hero from './components/Hero';
+import getLPTheme from './getLPTheme';
 
 function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
   return (
@@ -58,16 +62,25 @@ ToggleCustomTheme.propTypes = {
 };
 
 export default function LandingPage() {
+  const [mode, setMode] = React.useState('light');
   const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
 
   const toggleCustomTheme = () => {
     setShowCustomTheme((prev) => !prev);
   };
 
   return (
-    <>
+    <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+      <CssBaseline />
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+      <Hero />
       <Box sx={{ bgcolor: 'background.default' }}>
-        <Hero/>
         <LogoCollection />
         <Features />
         <Divider />
@@ -85,6 +98,6 @@ export default function LandingPage() {
         showCustomTheme={showCustomTheme}
         toggleCustomTheme={toggleCustomTheme}
       />
-    </>
+    </ThemeProvider>
   );
 }
