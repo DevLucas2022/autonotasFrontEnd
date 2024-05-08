f471f7fe4b5e8220984d112431cf0cac147a41d5
Luis <luis.lima42@fatec.sp.gov.br>
2024-05-08 09:43:14 -0300

    :sparkles: colocando app bar em todas as paginas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
 import React from 'react';
 import RoutesApp from './routes';
+import AppAppBar from './components/home/components/AppAppBar';
+import { Grid } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import getLPTheme from './components/home/getLPTheme';
+
+
 
 
 function App() {
+  const [mode, setMode] = React.useState('light');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
   return (
     <>
-      <RoutesApp />
+      <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
+          </Grid>
+          <Grid item xs={12}>
+            <RoutesApp />
+          </Grid>
+        </Grid>
+      </ThemeProvider>
     </>
   );
 }
