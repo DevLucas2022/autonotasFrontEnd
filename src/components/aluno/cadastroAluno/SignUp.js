451995884b153f894c1f1181b389c4389477f806
lucas santos <lucas.nascimento89@fatec.sp.gov.br>
2024-05-22 09:32:58 -0300

    footer changes

--- src/components/aluno/cadastroAluno/SignUp.js
+++ src/components/aluno/cadastroAluno/SignUp.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -55,11 +53,11 @@ export default function SignUpAluno() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            Cadastre-se
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12}>
                 <TextField
                   autoComplete="given-name"
                   name="nome"
@@ -80,7 +78,7 @@ export default function SignUpAluno() {
                   id="ra"
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
+              <Grid item xs={12} sm={12}>
                 <TextField
                   required
                   fullWidth
@@ -121,12 +119,6 @@ export default function SignUpAluno() {
                   id="cep"
                 />
               </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
             </Grid>
             <Button
               type="submit"
@@ -138,8 +130,8 @@ export default function SignUpAluno() {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
+                <Link href="/dashboard/aluno/login" variant="body2">
+                  Já possui uma conta? Entre
                 </Link>
               </Grid>
             </Grid>
