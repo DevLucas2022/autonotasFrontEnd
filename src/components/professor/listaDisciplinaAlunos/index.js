1520c73f0feddcc52a6910db1142495414155f5e
Luis Felipe <luis.lima42@fatec.sp.gov.br>
2024-06-04 21:12:47 -0300

    :sparkles: colocando app bar do professor

--- src/components/professor/listaDisciplinaAlunos/index.js
+++ src/components/professor/listaDisciplinaAlunos/index.js
@@ -83,7 +83,7 @@ function ListaDisciplinaAlunos() {
                 backgroundImage: 'url(/images/Background.png)'
             }}>
             <Grid container spacing={3} direction="row" justifyContent="center" alignItems="stretch">
-                <Grid item xs={12} marginBottom={5}><AppBarProfessor /></Grid>
+                <Grid item xs={12} marginBottom={8}><AppBarProfessor /></Grid>
                 <Grid item xs={6} md={6} lg={8} textAlign="right">
                     <Button onClick={handleAddAluno} variant="contained">Adicionar Aluno</Button>
                 </Grid>
